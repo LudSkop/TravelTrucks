03713a063ced917bb5ddff5b7dd6152fe572fbff
LudSkop <luda.skopenko80@gmail.com>
2026-07-22 19:25:12 +0300

    Customize project metadata and favicon

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import css from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className={css.container}>
+        <h1 className={css.title}>Campers of your dreams</h1>
+        <p className={css.description}>
+          You can find everything you want in our catalog
+        </p>
+        <button className={css.button}>View Now</button>
+      </div>
+    </main>
+  );
+}
