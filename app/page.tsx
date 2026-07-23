9cb9518697b3d77340aa013dd4df0ab0d4a31299
LudSkop <luda.skopenko80@gmail.com>
2026-07-23 16:30:04 +0300

    create home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import css from "./Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div className={css.container}>
-        <h1 className={css.title}>Campers of your dreams</h1>
-        <p className={css.description}>
-          You can find everything you want in our catalog
-        </p>
-        <button className={css.button}>View Now</button>
-      </div>
+      <section className={css.hero}>
+        <div className={css.container}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.description}>
+            You can find everything you want in our catalog
+          </p>
+          <Link href="/catalog" className={css.button}>
+            View Now
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
